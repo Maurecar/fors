a1d5761464c64bcb4a856c70c27b4033037e12d0
Maurecar <maurecar@gmail.com>
2023-12-18 01:59:48 +0000

    haciendo prueabs

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,23 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import resolve from '@rollup/plugin-node-resolve';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import externalGlobals from 'rollup-plugin-external-globals';
 
 export default defineConfig({
-  plugins: [vue(), resolve()],
+  plugins: [
+    vue(),
+    {
+      ...nodeResolve(),
+      dedupe: ['@fullcalendar/core'],
+    },
+    externalGlobals({
+      '@fullcalendar/core': 'FullCalendarCore',
+      '@fullcalendar/vue3': 'FullCalendarVue3',
+    }),
+  ],
   build: {
     rollupOptions: {
-      external: ['@fullcalendar/core/index.js', '@fullcalendar/core/internal.js', "@fullcalendar/core/preact.js"],
+      external: ['@fullcalendar/core', '@fullcalendar/vue3'],
     },
   },
 });
