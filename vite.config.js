914fbd1eb72a5ac01887288a664c9050e0c6e2b4
Maurecar <maurecar@gmail.com>
2023-12-18 01:33:36 +0000

    xc

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from '@rollup/plugin-node-resolve';
 
 export default defineConfig({
   plugins: [vue(), resolve()],
