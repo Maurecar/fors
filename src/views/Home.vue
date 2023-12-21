bcd417e059648d160d47cc0ff5d948496824b2b8
Maurecar <maurecar@gmail.com>
2023-12-21 08:05:04 -0500

    a ver si funciona

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,6 @@ import { useRouter } from 'vue-router'
 import { useSignOut, useUserId } from '@nhost/vue'
 
 import FullCalendar from '@fullcalendar/vue3'
-//import FullCalendar from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import { useQuery } from "@vue/apollo-composable";
@@ -57,30 +56,7 @@ const {
 }
 `);
 
-/* const mapToFullCalendarEvents = (reservation) => {
-  const events = [];
-
-  for (const n of reservation) {
-    events.push({
-      title: n.customer,
-      start: n.pickup_time,
-      end: n.re_pickup_time,
-      display: 'list-item',
-    });
-  }
-  return events;
-};
- */
-/* onMounted(async () => {
-  await reservationRefetch();
-
-  if (!reservationLoading.value && reservationResult.value) {
-    n.value = reservationResult.value.reservation || [];
-    calendarOptions.events = mapToFullCalendarEvents(reservationResult.value.reservations);
-  }
-});
- */
- const reservation = ref([]);
+const reservation = ref([]);
 
 onMounted(async () => {
   await reservationRefetch();
@@ -139,31 +115,6 @@ const logout = () => {
     </div>
   </main>
   <h1>Welcome to app </h1>
-  <!-- <div v-if="!reservationLoading">
-    <table class="table-auto">
-      <thead>
-        <tr>
-          <th class="px-4 py-2">Customer</th>
-          <th class="px-4 py-2">time</th>
-          <th class="px-4 py-2">landing_time</th>
-          <th class="px-2 py-1">date reser</th>
-
-          <th class="px-4 py-2">Actions</th>
-        </tr>
-      </thead>
-      <tbody v-if="reservationResult && reservationResult.reservation">
-
-        <tr v-for="n in reservationResult.reservation" :key="n.id">
-          <td class="border px-4 py-2">{{ n.customer }}</td>
-          <td class="border px-4 py-2">{{ n.pickup_time }}</td>
-          <td class="border px-4 py-2">{{ n.re_pickup_time }}</td>
-          <td class="border px-2 py-1">{{ n.vehicle}}</td>
-
-          <td class="border px-4 py-2"></td>
-        </tr>
-      </tbody>
-    </table>
-  </div> -->
   <div>
     <FullCalendar :options="calendarOptions" />
   </div>
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+/* import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
@@ -8,4 +8,21 @@ export default defineConfig({
       external: ['@fullcalendar/core/index.js'],
     },
   },
+  resolve: {
+    alias: {
+      '@fullcalendar/core': '@fullcalendar/core/vdom'
+    }
+  }
+}); */
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 });
